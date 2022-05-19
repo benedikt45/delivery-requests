66f52298117f5bd9895a61407f57da9a72b4097f
Benedikt <forcemaybeyouwith@gmail.com>
2022-05-20 00:42:47 +0500

    Remove Spinner

--- src/components/Spinner/Spinner.js
+++
@@ -1,8 +0,0 @@
-import { Spin } from 'antd';
-
-export const Spinner = () => (
-    <div className='spinner__wrapper'>
-        <div className='spinner__background'></div>
-        <Spin size='large' className='spinner' />
-    </div>
-);
